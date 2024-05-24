2c120ad194b039c2d0e2063cb47ef71d9f820b1e
[JuanDavid] <[juandavidrojas@javerianacali.edu.co]>
2024-05-24 10:56:09 -0500

    Se organizan de forma correcta las funciones

--- ASCCI_a_byte.py
+++ ASCCI_a_byte.py
@@ -3,10 +3,3 @@ def convert_to_ascii(byte):
     ascii_code = ord(ascii_char)
     return f"{ascii_char}-{ascii_code}"
 
-print("Menú de opciones:")
-option = int(input("Selecciona una opción (1): "))
-if option == 1:
-        byte = input("Ingresa un byte en formato binario (8 dígitos): ")
-        print("Representación ASCII:", convert_to_ascii(byte))
-else:
-    print("Opción inválida")
--- byte_a_palabra.py
+++ byte_a_palabra.py
@@ -2,16 +2,3 @@ def convert_to_byte(char):
     return ' '.join(format(ord(c), '08b') for c in char)
 
 
-print("Menú de opciones:")
-print("1. Generar representación en byte de un carácter")
-print("2. Generar representación en byte de una palabra")
-option = int(input("Selecciona una opción (1/2/3): "))
-
-if option == 1:
-        char = input("Ingresa un carácter: ")
-        print("Representación en byte:", convert_to_byte(char))
-elif option == 2:
-        word = input("Ingresa una palabra: ")
-        print("Representación en byte:", convert_to_byte(word))
-else:
-        print("Opción inválida")
